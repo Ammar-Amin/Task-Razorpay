ae1866e4af65d2a16964838f8bfcb67212238b30
Ammar-Amin <ammaramin5253@gmail.com>
2024-08-20 15:30:25 +0530

    install react & setup tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main className='w-full min-h-screen px-3'>
+      <h1 className='text-3xl md:text-5xl text-center py-5 md:py-8'>Razorpay Payment Gateway</h1>
+    </main>
+  )
+}
+
+export default App
